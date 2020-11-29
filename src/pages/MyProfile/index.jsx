7ac50c2f5add52dde87664274244c50646469d1c
satriayudhia <satria.yudhiap@gmail.com>
2020-11-29 21:09:50 +0700

    Fitur ganti foto profil

--- src/pages/MyProfile/index.jsx
+++ src/pages/MyProfile/index.jsx
@@ -14,18 +14,47 @@ import QR from '../../assets/logos/qr-code.png'
 const MyProfile = () => {
     const [state] = useContext(AppContext);
     const [user, setUser] = useState([])
+    const [image, setImage] = useState('')
 
     useEffect(() => {
         axios.get(`http://localhost:3000/users/${state.idUser}`)
             .then(function (response) {
                 // handle success
                 setUser(response.data)
+                setImage(response.data.profpic)
+                console.log("res", response.data)
             })
             .catch(function (error) {
                 // handle error
                 console.log(error);
             })
     }, [])
+
+    const uploadImage = async e => {
+        const files = e.target.files
+        const data = new FormData()
+        data.append('file', files[0])
+        data.append('upload_preset', 'satriayud')
+        // setLoading(true)
+        const res = await fetch(
+            'https://api.cloudinary.com/v1_1/satria-img/image/upload',
+            {
+                method: 'POST',
+                body: data
+            }
+        )
+        const file = await res.json()
+        setImage(file.secure_url)
+        // setLoading(false)
+        console.log("result user id", user.id)
+        axios({
+            method: 'patch',
+            url: `http://localhost:3000/users/${user.id}`,
+            data: {
+                profpic: file.secure_url
+            }
+        })
+    }
 
     return (
         <Container fluid>
@@ -36,7 +65,10 @@ const MyProfile = () => {
             </Row>
             <Row className="content-profile">
                 <Col sm={2}>
-                    <img className="profpic" src="https://s3-alpha-sig.figma.com/img/5736/4ad6/6ebf751e7f102311d3ba8137ef22382a?Expires=1607299200&Signature=QQrPC5JP-QJaAlY1uI3D6y18mLnCVQzFyl~vG~pQ3PX4-l-p8ygi4pcnaQNdhE1wICmViRKiZt6d5z-IgWhYb8aozUj0Slmfxmldn1sgAZ4kHpGPYkAr3GsxgBS6vKT3B6mW4jdbvBECpbt6vxctuWGnTmG2trWAcHK~AcxQzIyVs4O6~KgNjcWmkiVpP3lRkKwj5EJShFKazrb6fTmZcqeATLAwDoM1Mt~ivUMguQJCslrlO7IjYRAbFVyjnPDtzDPvniIPTRvbVK2m7GIO6lR6scxFfLH5rRhP6prYMsZFJxem2UzmkLo~HkcCdT2x-r5ktGLfSNlB-Fh3Vhuv5w__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA" alt="Profile Picture" />
+                    <input id="upload-img" onChange={uploadImage} className="file-input-img" type="file" placeholder="Photo Product"/>
+                    <label for="upload-img">
+                        <img className="profpic" src={image} alt="Profile Picture" />
+                    </label>
                 </Col>
                 <Col className="profile-detail" sm={4}>
                     <Row className="brown-text">Fullname</Row>
